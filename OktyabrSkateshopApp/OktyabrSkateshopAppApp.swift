aad26b3dffb1a02883e2dcf2d3d4a20da8e50c8a
Andrew Buyanov <dantees1a@gmail.com>
2024-04-15 19:27:22 +0800

    Initial Commit

---
+++ OktyabrSkateshopApp/OktyabrSkateshopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OktyabrSkateshopAppApp.swift
+//  OktyabrSkateshopApp
+//
+//  Created by Андрей Буянов on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct OktyabrSkateshopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
